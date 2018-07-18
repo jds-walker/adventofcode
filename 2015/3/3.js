7e0cbe270dda98ee0f702caf069419209b4bb215
Jonathan Walker <jds_walker@optusnet.com.au>
2018-07-18 17:39:20 +1000

    progress to coordinates now need to map reduce to unique coordinates + count of instances

--- 2015/3/3.js
+++ 2015/3/3.js
@@ -21,15 +21,30 @@ function movement(direction) {
 			} 
 }
 
+
+
+
+
 function question1() {
 	fs.readFile('./2015/3/3.txt', (err, data) => {
 		input = data.toString();
 		split = input.split('');
 		const move = split.map(movement);
 		let coordinates = []
-		move.reduce(function(a,b,i) { return coordinates[i] = a+b; },0)
-		})
-	console.log(coordinates)
+		const reduce = move.reduce((acc,arr) => {
+			acc = [acc[0]+arr[0],acc[1]+arr[1],acc[2]+arr[2]]
+			coordinates.push(acc)
+			return acc
+		},[0,0,0]) 
+		const map = coordinates.map(arr => {
+				
+				return arr
+			})
+			console.log(map)
+			}
+		)
+	
+
 }
 
 
